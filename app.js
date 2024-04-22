9fec4c739086c73db851d3af6a78566e03c9dcc5
okashabib-oh <okasha.qavitech786@gmail.com>
2024-04-22 12:53:49 +0500

    changes

--- app.js
+++ app.js
@@ -1,38 +1,3 @@
-// const form = document.getElementById('namazForm');
-// const input = document.getElementById('cityname');
-// const result = document.getElementById('result-container');
-
-// form.addEventListener('submit', (event) => {
-//     event.preventDefault()
-
-//     const date = new Date()
-
-//     const day = date.getDate()
-//     const month = date.getMonth() + 1
-//     const year = date.getFullYear()
-
-//     const fullDate = `${day}-${month}-${year}`
-
-
-//     const cityName = input.value;
-    
-//         const api = fetch(`https://api.aladhan.com/v1/timingsByCity/${fullDate}?city=${cityName}&country=%27%27&method=8`)
-//             .then(res => res.json())
-//                 .then(data => {
-//                     const namazTimes = data.data.timings
-//                     result.innerHTML = `
-//                     <h1>Namaz times for ${cityName} on ${fullDate}</h1>
-//                     <b>FAJR: ${namazTimes.Fajr}</b><hr>
-//                     <b>DHUHR: ${namazTimes.Dhuhr}</b><hr>
-//                     <b>ASR: ${namazTimes.Asr}</b><hr>
-//                     <b>MAGHRIB: ${namazTimes.Maghrib}</b><hr>
-//                     <b>ISHA: ${namazTimes.Isha}</b><hr>
-//                     `
-//                 })
-
-// })
-
-
 const form = document.getElementById('namazForm');
 const input = document.getElementById('cityname');
 const resultContainer = document.getElementById('result-container');
